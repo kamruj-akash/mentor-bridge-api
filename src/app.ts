eb631c0dc6837bddae9352e2bbe77c40a09c7330
kamruj-akash <source.akash@gmail.com>
2026-09-02 11:28:17 +0600

    style: format code for consistency and readability

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-import cors from 'cors';
-import express, { type Application } from 'express';
+import cors from "cors";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
 
-const app : Application  = express();
+const app: Application = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Hello MentorBridge V1.0!" });
+});
 
 export default app;
