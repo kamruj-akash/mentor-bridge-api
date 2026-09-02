b7491cb3ee7569a5cd62bdd47cf3c03ef14aee3e
kamruj-akash <source.akash@gmail.com>
2026-09-02 11:14:36 +0600

    feat: initialize project with Express, TypeScript, and Bun
    - Added package.json with scripts and dependencies
    - Created app.ts to set up Express server with CORS and JSON parsing
    - Implemented server.ts to start the Express server on a specified port
    - Configured tsconfig.json for TypeScript with strict settings and bundler mode

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import app from "./app";
+
+const PORT = process.env.PORT || 3000;
+async function server(){
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port https://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error('Error in main function:', error);
+        process.exit(1);
+    }
+}
+
+server()
